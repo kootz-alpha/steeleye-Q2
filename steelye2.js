541d3f49a87ace569113aa85161d5e275b09277b
kootz-alpha <101920127+kootz-alpha@users.noreply.github.com>
2023-08-06 00:26:29 +0530

    Added comments

--- steelye2.js
+++ steelye2.js
@@ -1,5 +1,15 @@
+// Function to get the corresponding position of words from plainText in htmlContent
 function getHTMLvsPlanTextArray(htmlContent, plainText) {
 
+    /*
+    htmlContent is traversed and everything between '>' and '<' is analysed.
+    As things in plainText and htmlContent would be in the same order assuming that the inputs are always accurate, we don't
+    have to check for equality.
+    If a ' ' or '<' is found then the position of the prevoius word is stored.
+
+    The output is an array of arrays, where each inner array is of length 2 and contains the starting index of the word in plainText and htmlContent
+    */
+    
     let i = 0, j = 0, htmlStart = -1, plainStart = -1, n = htmlContent.length, m = plainText.length;
     let opened = false;
 
@@ -69,7 +79,14 @@ function getHTMLvsPlanTextArray(htmlContent, plainText) {
     return ans;
 }
 
+// Function to construct the final output string
 function solve(htmlVsPlainTextArray, plainTextPositions, htmlContent) {
+
+    /*
+    We traverse through plainTextPositions while assuming it will always be in the order of occurence of the words in the original string
+    For every element in plainTextPositions we find it's starting position from htmlVsPlainTextArray and add "<mark>" and "</mark>" before and
+    after it respectively to highlight the word.
+    */
 
     let res = "";
     let j = 0, n = htmlVsPlainTextArray.length, m = plainTextPositions.length, starti = 0;
@@ -106,6 +123,7 @@ function highlightHTMLContent(htmlContent, plainText, plainTextPositions) {
     return solve(htmlVsPlainTextArray, plainTextPositions, htmlContent);
 }
 
+/*
 const htmlContent = ' <p><span>Hi David<br><br>Headline: Energix Closes $520 Million Financing and Tax Equity Deal to Fund New Solar Projects<br><br>Summary: Two deals with Morgan Stanley Renewables Inc. and Santander CIB will help finance the construction and operation of six utility Equity scale solar…<br><br>Read the full article <a href="https://content.seleritycorp.com/hosted/assets/www/UKMW47_hYz_RGzPSpHm44Hi1L49HdNBhs1OkKKW2OPI">here</a><br><br>-------------------------------------<br><br>You received this because you are subscribed to news related to <a href="https://iris.steeleye.co/market/instruments?search=ES0113900J37">ES0113900J37</a>, and this story was marked as 82% relevant.<br><br>Copyright of PR Newswire. All Rights Reserved. Terms and Conditions | Privacy Policy. To stop PR Newswire emails from getting removed by email filters please add our address (noreply@prnewswire.com) to your email address book. Registered Office: 3 Spring Mews, London SE11 5AN. Tel: +44 (0) 207 8405100. <br><br>To unsubscribe change your email preferences, please click <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley">here</a>.<br><br>-------------------------------------<br><br><img src="https://context.seleritycorp.com/selerity/assets/sc_icons/pressRelease.png" alt="Rick Astley" style="width:100px;height:100px;"></span></p>';
 const plainText = 'Hi David Headline: Energix Closes $520 Million Financing and Tax Equity Deal to Fund New Solar Projects Summary: Two deals with Morgan Stanley Renewables Inc. and Santander CIB will help finance the construction and operation of six utility Equity scale solar… Read the full article here ------------------------------------- You received this because you are subscribed to news related to ES0113900J37 , and this story was marked as 82% relevant. Copyright of PR Newswire. All Rights Reserved. Terms and Conditions | Privacy Policy. To stop PR Newswire emails from getting removed by email filters please add our address (noreply@prnewswire.com) to your email address book. Registered Office: 3 Spring Mews, London SE11 5AN. Tel: +44 (0) 207 8405100. To unsubscribe change your email preferences, please click here . -------------------------------------';
 const plainTextPositions = [
@@ -121,3 +139,5 @@ const plainTextPositions = [
 
 const highlightedContent = highlightHTMLContent(htmlContent, plainText, plainTextPositions);
 console.log(highlightedContent);
+
+*/
